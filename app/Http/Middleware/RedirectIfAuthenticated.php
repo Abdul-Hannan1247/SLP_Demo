6bc59cf28c573a1cd1e81dec5139e957e1392742
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-30 04:22:17 +0500

    Admint Panel setup

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -64,7 +64,6 @@ class RedirectIfAuthenticated
         }
 
 
-
         return '/';
     }
 
--- routes/admin.php
+++ routes/admin.php
@@ -9,9 +9,10 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["middleware"=> "guest:admin","prefix"=>"admin","as" =>"admin."],function () {
+Route::group(["middleware" => "guest:admin", "prefix" => "admin", "as" => "admin."], function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
@@ -35,7 +36,7 @@ Route::group(["middleware"=> "guest:admin","prefix"=>"admin","as" =>"admin."],fu
         ->name('password.store');
 });
 
-Route::group(["middleware"=>'auth:admin',"prefix"=>"admin", "as"=>"admin."], function () {
+Route::group(["middleware" => 'auth:admin', "prefix" => "admin", "as" => "admin."], function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
@@ -56,4 +57,6 @@ Route::group(["middleware"=>'auth:admin',"prefix"=>"admin", "as"=>"admin."], fun
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
