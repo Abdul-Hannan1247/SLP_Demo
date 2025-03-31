bbde1b7ffee52323aee35aa6102ba63faf13a801
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-31 12:37:13 +0500

    Dynamic User will be displayd on header of admin/dashboard

--- resources/views/Admin/layouts/header.blade.php
+++ resources/views/Admin/layouts/header.blade.php
@@ -158,8 +158,8 @@
                     aria-label="Open user menu">
                     <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
                     <div class="d-none d-xl-block ps-2">
-                        <div>Paweł Kuna</div>
-                        <div class="mt-1 small text-secondary">UI Designer</div>
+                        <div>{{ Auth::user()->name }}</div>
+                        <div class="mt-1 small text-secondary">{{ Auth::user()->role ?? 'Admin' }}</div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
